239705fb005c6c4e7abf1b87de441829ad7a6d85
berth21gg <19031032@itcelaya.edu.mx>
2024-05-16 11:19:07 -0600

    Uso de Fetching Data para traer datos de la api con json-server

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 import CarouselClient from "./components/CarouselClient";
 
+
 export default function Home() {
   return (
-    <main class="container-fluid pt-5">
+    <main className="container-fluid pt-5">
       <CarouselClient/>
     </main>
   );
--- app/scooter/page.js
+++ app/scooter/page.js
@@ -1,7 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.css'
-export default function Page() {
+
+async function getProducts() {
+    const res = await fetch('http://localhost:4000/products')
+   
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+   
+    return res.json()
+  }
+
+export default async function Scooter(){
+    const products = await getProducts()
+    console.log(products)
+
     return(
-        <main class="contPrincipal">
+        <main className="contPrincipal">
             <h1>Scooter page</h1>
         </main>
     )
